994933c4e3875ecf2b2ebd746577de0eb69c853b
teguh.praasetyo <teguh.praasetyo@gmail.com>
2025-07-28 16:57:31 +0000

    fix: Hapus proxy dev dari vite config

--- vite.config.js
+++ vite.config.js
@@ -9,15 +9,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  server: {
-    proxy: {
-      // Setiap request di frontend yang dimulai dengan '/api'
-      // akan diteruskan ke server backend Anda.
-      '/api': {
-        target: 'http://localhost:3000', // Server backend berjalan di port 3000
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // Hapus '/api' dari path
-      },
-    }
-  }
 })
